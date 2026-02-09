b09a6ff76a15ebdd51a3481a6061328d7af66944
LucS0MA <lucien.kohli@gmail.com>
2026-02-09 23:07:09 +0100

    added auth middleware that check the validity of the jwt token

--- src/types/express.types.ts
+++ src/types/express.types.ts
@@ -3,9 +3,6 @@ import { JwtPayload } from "./jwtPayload.types.js";
 declare global {
   namespace Express {
     interface Request {
-      cookies: {
-        token?: string;
-      };
       user?: JwtPayload;
     }
   }
